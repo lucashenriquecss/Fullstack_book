623b5e7c801b697edf741123afa36bb78e9135fb
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-08-18 23:12:31 -0300

    separando rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,71 @@ import AddTodo from './components/Todo/AddTodo';
 import TodosList from './components/Todo/Todolist';
 import Login from './components/User/Login';
 import SignUp from './components/User/SignUp';
-
+import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Navbar';
 
 function App() {
+  const [user,setUser] = React.useState(null);
+  const [token,setToken] = React.useState(null);
+  const [error,setError] = React.useState('');
+
+  async function login(user = null){
+    //default user to null
+    setUser(user);
+  }
+  async function logout(){
+    setUser(null);
+  }
+  async function signup(user = null){
+    //default user to null
+    setToken(user);
+  };
+
+
+
   return (
     <div className="App">
       <Navbar bg="primary" variant="dark">
         <div className="container-fluid">
-          <Navbar.Brand>React-bootstrap</Navbar.Brand>
+          <Navbar.Brand>Todo App</Navbar.Brand>
           <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+            <Container>
+              <Link class="nav-link" to ={"/todo"}>Todos</Link>
+              { user ? (
+                <Link class="nav-link">Logout({user})</Link>
+              ):(
+                <>
+                  <Link class="nav-link" to = {"/login"}>Login</Link>
+                  <Link class="nav-link" to ={"/signup"}>Signup</Link>
+                </>
+              )}
+            </Container>
+          
           </Nav>
         </div>
       </Navbar>
+      <div className="container mt-4">
+              <Switch>
+                <Route exact path={["/","todos"]} render={(props)=>
+                  <TodosList {...props} token={token}/>
+                }></Route>
+                <Route path="/todos/create" render={(props)=>
+                  <AddTodo {...props} token={token}/>
+                }></Route>
+                <Route path="/todos/:id/" render={(props)=>
+                  <AddTodo {...props} token={token}/>
+                }></Route>
+                <Route path="/login" render={(props)=>
+                  <Login {...props} login={login}/>
+                }></Route>
+                <Route path="/signup" render={(props)=>
+                  <SignUp {...props} signup={signup}/>
+                }></Route>
+              </Switch>
+
+      </div>
     </div>
   );
 }
